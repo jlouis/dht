fd088e7e21d46674768e5a2a43238449fa2df0ce
Uvarov Michael <freeakk@gmail.com>
2012-01-10 16:02:56 +0400

    Fix running of etorrent without building a release.
    Allow to start with start-dev.sh without building a release.
    
    Port changes.

---
+++ README.md
@@ -0,0 +1,8 @@
+How to run
+==========
+
+```
+rebar get-deps compile
+cp "priv/config.example" "~/.config/etorrent.config"
+./start-dev.sh
+```
--- src/etorrent_app.erl
+++ src/etorrent_app.erl
@@ -16,23 +16,26 @@
 -export([start/2, stop/1, prep_stop/1, profile_output/0]).
 
 -define(RANDOM_MAX_SIZE, 999999999999).
+-define(APP, etorrent_core).
 
 start() ->
     start([]).
 
 start(Config) ->
     load_config(Config),
-    {ok, Deps} = application:get_key(etorrent, applications),
+    % Load app file.
+    application:load(?APP),
+    {ok, Deps} = application:get_key(?APP, applications),
     true = lists:all(fun ensure_started/1, Deps),
-    application:start(etorrent).
+    application:start(?APP).
 
 stop() ->
-    application:stop(etorrent).
+    application:stop(?APP).
 
 load_config([]) ->
     ok;
 load_config([{Key, Val} | Next]) ->
-    application:set_env(etorrent, Key, Val),
+    application:set_env(?APP, Key, Val),
     load_config(Next).
 
 %% @private
@@ -85,6 +88,8 @@ stop(_State) ->
     ok.
 
 start_webui() ->
+    cascadae:start(),
+
     Dispatch = [ {'_', [{'_', etorrent_cowboy_handler, []}]} ],
     {ok, _Pid} =
         cowboy:start_listener(http, 10,
