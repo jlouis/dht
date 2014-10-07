604d899bff161b5057a94c09760a65c3cd284c32
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-10-07 20:22:00 +0200

    implement a simple in-memory store based on ETS for now.

--- src/dht_store.erl
+++ src/dht_store.erl
@@ -4,17 +4,38 @@
 %% lifetime API
 -export([start_link/0]).
 
+%% Operational API
+-export([store/2, find/1]).
+
 %% gen_server API
 -export([init/1, handle_cast/2, handle_call/3, terminate/2, code_change/3, handle_info/2]).
+
+-define(TBL, ?MODULE).
+
+-record(state, { tbl }).
 
 %% API
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+store(ID, {IP, Port}) ->
+    gen_server:call(?MODULE, {store, ID, {IP, Port}}).
+    
+find(ID) ->
+    gen_server:call(?MODULE, {find, ID}).
+
 %% Callbacks
 init([]) ->
-    {ok, ok}.
+    Tbl = ets:new(?TBL, [named_table, protected, {type, bag}]),
+    {ok, #state { tbl = Tbl }}.
 	
+handle_call({store, ID, Peer}, _From, State) ->
+    Now = dht_time:timestamp(),
+    ets:insert(?TBL, {ID, Peer, Now}),
+    {reply, ok, State};
+handle_call({find, Key}, _From, State) ->
+    Peers = ets:match(?TBL, {Key, '$1', '_'}),
+    {reply, [ID || [ID] <- Peers], State};
 handle_call(_Msg, _From, State) ->
     {reply, {error, unknown_msg}, State}.
 	
