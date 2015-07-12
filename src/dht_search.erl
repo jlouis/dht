0cc35faf9b6f7e0da7a677088c152e5ad9374b48
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-07-12 11:44:07 +0200

    Run a refactoring/simplification round on the search code.

--- src/dht_search.erl
+++ src/dht_search.erl
@@ -8,6 +8,13 @@
 -define(SEARCH_WIDTH, 32).
 -define(SEARCH_RETRIES, 4).
 
+-record(search_state, {
+    query_type :: find_node | find_value,
+    done = gb_sets:empty(),
+    alive = gb_sets:empty(),
+    acc = []
+}).
+
 %% SEARCH API
 %% ---------------------------------------------------
 
@@ -19,69 +26,99 @@ find(Type, ID) ->
 %% -----------------------------
 
 search_iterate(QType, Target, Width, Nodes)  ->
-	NodeID = dht_state:node_id(),
-    dht_iter_search(NodeID, QType, Target, Width, ?SEARCH_RETRIES, Nodes,
-                    gb_sets:empty(), gb_sets:empty(), []).
+    NodeID = dht_state:node_id(),
+    dht_iter_search(NodeID, Target, Width, ?SEARCH_RETRIES, Nodes,
+        #search_state{ query_type = QType }).
 
-dht_iter_search(_NodeID, find_node, _Target, _Width, 0, _Todo, _Done, Alive, _Acc) ->
-    gb_sets:to_list(Alive);
-dht_iter_search(_NodeID, find_value, _Target, _Width, 0, _Todo, _Done, Alive, Acc) ->
-    Store = [{N, Token} || {N, Token, _Vs} <- Acc],
-    Found = [V || {_, _, Vs} <- Acc, V <- Vs],
-    {Store, Found, gb_sets:to_list(Alive)};
-dht_iter_search(NodeID, QType, Target, Width, Retries, Todo, Done, Alive, Acc) ->
-
+dht_iter_search(_NodeID, _Target, _Width, 0, _Todo, #search_state { query_type = find_node } = State) ->
+    alive(State);
+dht_iter_search(_NodeID, _Target, _Width, 0, _Todo, #search_state { query_type = find_value } = State ) ->
+    Res = res(State),
+    #{
+      store => [{N, Token} || {N, Token, _Vs} <- Res],
+      found => [V || {_, _, Vs} <- Res, V <- Vs],
+      alive => alive(State)
+    };
+dht_iter_search(NodeID, Target, Width, Retries, Todo, #search_state{ query_type = QType } = State) ->
     %% Call the DHT in parallel to speed up the search:
-    Call = fun({_, IP, Port} = N) -> {N, apply(dht_net, QType, [IP, Port, Target])} end,
+    Call = fun({_, IP, Port} = N) ->
+        {N, apply(dht_net, QType, [{IP, Port}, Target])}
+    end,
     Results = dht_par:pmap(Call, Todo),
 
-    %% Maintain invriants for the next round by updating the necessary data structures:
-
-    %% Mark all nodes that responded as alive
-    Living = gb_sets:union(Alive, gb_sets:from_list(alive_nodes(Results))),
-    Queried = gb_sets:union(Done, gb_sets:from_list(Todo)),
-    New = [N || N <- all_nodes(Results),
-                not gb_sets:is_member(N, Queried)],
+    %% Maintain invariants for the next round by updating the necessary data structures:
+    #{ new := New, next := NextState } = track_state(Results, Todo, State),
+    
     WorkQueue = lists:usort(dht_metric:neighborhood(Target, New, Width)),
-    Retry =
-      case view_closest_node(NodeID, Living, WorkQueue) of
-        work_queue -> ?SEARCH_RETRIES;
-        alive_set -> Retries - 1
-      end,
-    Found = accum_peers(QType, Acc, Results),
+    Retry = update_retries(NodeID, Retries, WorkQueue, NextState),
+    dht_iter_search(NodeID, Target, Width, Retry, WorkQueue, NextState).
 
-    dht_iter_search(NodeID, QType, Target, Width, Retry, WorkQueue, Queried, Living, Found).
+update_retries(NodeID, K, WorkQueue, State) ->
+      case view_closest_node(NodeID, alive(State), WorkQueue) of
+          work_queue -> ?SEARCH_RETRIES;
+          alive_set -> K - 1
+      end.
+    
+%% Once a round completes, track the state of the round in the #search_state{} record.
+%%
+%% Rules:
+%% • Nodes which responded as being alive are added to the alive-set
+%% • Track which nodes has been processed
+%% • Accumulate the results
+%%
+%% Returns the next state as well as the set of newly found nodes (for the work-queue)
+%%
+track_state(Results, Processed,
+	#search_state {
+		query_type = QType,
+		alive = Alive,
+		done = Done,
+		acc = Acc } = State) ->
+    Next = State#search_state {
+        alive = gb_sets:union(Alive, gb_sets:from_list( alive_nodes(Results)) ),
+        done = Queried = gb_sets:union(Done, gb_sets:from_list(Processed)), 
+        acc = accum_peers(QType, Acc, Results)
+    },
+    New = [N || N <- all_nodes(Results), not gb_sets:is_member(N, Queried)],
+    
+    #{ new => New, next => Next }.
+
+%% Select nodes from a response
+resp_nodes({nodes, _, Ns}) -> Ns;
+resp_nodes(_) -> [].
 
 %% all_nodes/1 gathers nodes from a set of parallel queries
-all_nodes(Resp) ->
-    Nodes = fun ({nodes, _, Ns}) -> Ns;
-                (_) -> []
-            end,
-    lists:concat([Nodes(R) || {_N, R} <- Resp]).
+all_nodes(Resp) -> lists:concat([resp_nodes(R) || {_N, R} <- Resp]).
     
+%% Compute if a response is ok, for use in a predicate
+ok({error, _}) -> false;
+ok({error, _ID, _Code, _Msg}) -> false;
+ok(_) -> true.
+
 %% alive_nodes/1 returns the nodes returned positive results
-alive_nodes(Resp) ->
-    OK = fun ({error, _}) -> false;
-             ({error, _ID, _Code, _Msg}) -> false;
-             (_) -> true
-         end,
-    [N || {N, R} <- Resp, OK(R)].
+alive_nodes(Resp) -> [N || {N, R} <- Resp, ok(R)].
 
 %% accum_peers/2 accumulates new targets with the value present
 accum_peers(find_node, [], _Results) -> []; % find_node never has results to accumulate
 accum_peers(find_value, Acc, Results) ->
-    New = [{Node, Token, Vs} || {Node, {values, _, Token, Vs}} <- Results,
-                                   Vs /= [] ],
+    New = [{Node, Token, Vs} || {Node, {values, _, Token, Vs}} <- Results, Vs /= [] ],
     New ++ Acc.
 
-%% Check if the closest node in the work queue is closer
-%% to the target than the closest responsive node that was
-%% found in this iteration.
+%% view_closest_node/3 determines if the closest node is in the work_queue or in the alive set
 view_closest_node(ID, AliveNodes, WorkQueue) ->
-    D = fun({NID, _, _}, M) -> min(M, dht_metric:d(ID, NID)) end,
-    MinA = gb_sets:fold(D, infinity, AliveNodes),
-    MinQ = lists:foldl(D, infinity, WorkQueue),
-    case MinQ < MinA of
+    DistFun = fun({NID, _, _}, Incumbent) ->
+        min(dht_metric:d(ID, NID), Incumbent)
+    end,
+
+    MinAlive = gb_sets:fold(DistFun, infinity, AliveNodes),
+    MinWork = lists:foldl(DistFun, infinity, WorkQueue),
+    case MinWork < MinAlive of
         true -> work_queue;
         false -> alive_set
     end.
+
+%% find the nodes/values which are alive
+alive(#search_state { alive = A }) -> maps:to_list(A).
+
+%% obtain the final result of the query
+res(#search_state { acc = A }) -> A.
