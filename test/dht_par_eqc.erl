d2d75c8ad0c384d112b80b24f1e11d442e48eaa8
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-10-20 16:58:32 +0200

    Do not use a stateful test when a stateless test will do.

--- test/dht_par_eqc.erl
+++ test/dht_par_eqc.erl
@@ -3,14 +3,8 @@
 -compile(export_all).
 
 -include_lib("eqc/include/eqc.hrl").
--include_lib("eqc/include/eqc_statem.hrl").
 -include_lib("pulse/include/pulse.hrl").
 
-initial_state() -> {}.
-
-%% The parameter generator
-gen() ->
-    int().
 
 pmap(F, Params) ->
     dht_par:pmap(F, Params).
@@ -22,25 +16,19 @@ crasher() ->
        (X) -> F(X)
      end).
 
-pmap_args(_S) ->
-    [crasher(),
-     list( frequency([ {10, 0}, {100, int()} ]) )].
-    
-pmap_return(_S, [F, Xs]) ->
+expected_result(F, Xs) ->
     [case X of
          0 -> {error, err};
          N -> {ok, F(N)}
      end || X <- Xs].
-    
-postcondition_common(S, Call, Res) ->
-    eq(Res, return_value(S, Call)).
 
-prop_component_correct() ->
-    ?FORALL(Cmds, commands(?MODULE),
-      ?PULSE(HSR = {_,_,R}, run_commands(?MODULE, Cmds),
+prop_pmap() ->
+    ?FORALL([F, Xs], [crasher(), list( frequency([ {10,0}, {100, int()} ]) ) ],
+      ?PULSE(Result, dht_par:pmap(F, Xs),
         begin
-          pretty_commands(?MODULE, Cmds, HSR, R == ok)
-        end)).
+            Expected = expected_result(F, Xs),
+            equals(Result, Expected)
+        end).
 
 pulse_instrument() ->
   [ pulse_instrument(File) || File <- filelib:wildcard("../src/dht_par.erl") ++ filelib:wildcard("../test/dht_par_eqc.erl") ],
