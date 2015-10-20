470d86bcb9aa17c579be955859712ab29681cd91
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-10-20 18:42:46 +0200

    Go with PULSE, handle timer crashes.

--- test/dht_par_eqc.erl
+++ test/dht_par_eqc.erl
@@ -12,23 +12,25 @@ pmap(F, Params) ->
 crasher() ->
    ?LET(F, function1(int()),
      fun
-       (0) -> exit(err);
-       (X) -> F(X)
+       (-1) -> timer:sleep(6000); %% Fail by timing out
+       (0) -> exit(err); %% Fail by crashing
+       (X) -> F(X) %% Run normally
      end).
 
 expected_result(F, Xs) ->
-    [case X of
-         0 -> {error, err};
-         N -> {ok, F(N)}
-     end || X <- Xs].
+    case [X || X <- Xs, X == -1] of
+        [] -> [case X of 0 -> {error, err}; N -> {ok, F(N)} end || X <- Xs];
+        [_|_] ->
+            {'EXIT', pmap_timeout}
+    end.
 
 prop_pmap() ->
-    ?FORALL([F, Xs], [crasher(), list( frequency([ {10,0}, {100, int()} ]) ) ],
-      ?PULSE(Result, dht_par:pmap(F, Xs),
+    ?FORALL([F, Xs], [crasher(), list( frequency([ {10,0}, {1, -1}, {1000, nat()} ]) ) ],
+      ?PULSE(Result, (catch dht_par:pmap(F, Xs)),
         begin
             Expected = expected_result(F, Xs),
             equals(Result, Expected)
-        end).
+        end)).
 
 pulse_instrument() ->
   [ pulse_instrument(File) || File <- filelib:wildcard("../src/dht_par.erl") ++ filelib:wildcard("../test/dht_par_eqc.erl") ],
